0b8dc22154fb9105956518b7d024ab42f4820b84
Rafael Santos Telles <rafast.telles@gmail.com>
2022-05-07 18:43:05 -0300

    * .gitignore: Primeiro commit
    * ListaExercicios02.sln:
    * Program.cs:
    * ListaExercicios02.csproj:

---
+++ ListaExercicios02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListaExercicios02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
